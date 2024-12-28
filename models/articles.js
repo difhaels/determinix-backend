dc14b09e1719074596388a75ca464fe5d90d9013
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-28 21:14:56 +0700

    fix bug

--- models/articles.js
+++ models/articles.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const articlestSchema = new mongoose.Schema({
   title: String,
   date: String,
-  writer: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
+  writer: { type: mongoose.Schema.Types.ObjectId, ref: 'Member' },
   type: String,
   short: String,
   img: String,
--- seed.js
+++ seed.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
-const Articles = require('./models/articles'); // Asumsi Anda memiliki model Project
+const mongoose = require("mongoose");
+const Articles = require("./models/articles"); // Asumsi Anda memiliki model Project
 
 const articlesData = [
   {
     title: "Articles 01",
-    writer: [
-      new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0ba"),
-    ],
+    writer: new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0ba"),
     date: "May 18, 2023",
     type: "GENERAL",
     short:
@@ -15,9 +13,7 @@ const articlesData = [
   },
   {
     title: "Articles 02",
-    writer: [
-      new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0bc"),
-    ],
+    writer: new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0bc"),
     date: "May 18, 2023",
     type: "GENERAL",
     short:
@@ -26,10 +22,8 @@ const articlesData = [
   },
   {
     title: "Articles 03",
-    writer: [
-      new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0ba"),
-      new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0bb"), // ID Member 2
-    ],
+    writer: new mongoose.Types.ObjectId("676e43065c3d5e7d283dc0bb"),
+
     date: "May 18, 2023",
     type: "GENERAL",
     short:
@@ -38,20 +32,19 @@ const articlesData = [
   },
 ];
 
-
 const insertProjects = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/determinix', {
+    await mongoose.connect("mongodb://localhost:27017/determinix", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
     const result = await Articles.insertMany(articlesData);
-    console.log('Projects inserted:', result);
+    console.log("Projects inserted:", result);
 
     mongoose.disconnect(); // Tutup koneksi setelah selesai
   } catch (error) {
-    console.error('Error inserting projects:', error.message);
+    console.error("Error inserting projects:", error.message);
   }
 };
 
