c74ef4f216895e12b4a244de2f32e80a8dedd1b0
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-28 18:05:04 +0700

    activities id endpoint

--- server.js
+++ server.js
@@ -61,6 +61,23 @@ app.get('/projects/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+
+});
+// Endpoint untuk mendapatkan activity berdasarkan ID
+app.get('/activities/:id', async (req, res) => {
+  const { id } = req.params; // Mendapatkan ID dari parameter URL
+  try {
+    // Menemukan proyek berdasarkan ID
+    const activity = await Activities.findById(id)
+    
+    if (!activity) {
+      return res.status(404).json({ message: 'Activity not found' });
+    }
+    
+    res.json(activity); // Mengirimkan data proyek sebagai respons
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 
