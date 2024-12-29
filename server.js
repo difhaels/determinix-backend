1459c43e8bf41b2055d0c37097c52f16325c782b
Agung Saputra <agungsaputradifh@gmail.com>
2024-12-29 07:07:58 +0700

    ep member id

--- server.js
+++ server.js
@@ -57,6 +57,23 @@ app.get('/articles', async (req, res) => {
   }
 });
 
+// Endpoint untuk mendapatkan member berdasarkan ID
+app.get('/member/:id', async (req, res) => {
+  const { id } = req.params; // Mendapatkan ID dari parameter URL
+  try {
+    // Menemukan proyek berdasarkan ID dan meng-populate data anggota
+    const project = await Member.findById(id);
+    
+    if (!project) {
+      return res.status(404).json({ message: 'Member not found' });
+    }
+    
+    res.json(project); // Mengirimkan data proyek sebagai respons
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // Endpoint untuk mendapatkan project berdasarkan ID
 app.get('/projects/:id', async (req, res) => {
   const { id } = req.params; // Mendapatkan ID dari parameter URL
@@ -72,7 +89,6 @@ app.get('/projects/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-
 });
 
 // Endpoint untuk mendapatkan activity berdasarkan ID
