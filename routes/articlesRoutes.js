e29fb0c5c615e8f7eaf75dad5b2a25eedd6e9982
Agung Saputra <agungsaputradifh@gmail.com>
2025-01-29 07:35:38 +0700

    prefix articles

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -2,9 +2,58 @@ const express = require("express");
 const router = express.Router();
 const Articles = require("../models/articles");
 
+// mendapatkan semua artikel
 router.get("/", async (req, res) => {
   try {
     const articles = await Articles.find().populate("writer");
+    res.json(articles);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// mendapatkan artikerl berdasarkan id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params; // Mendapatkan ID dari parameter URL
+  try {
+    const activity = await Articles.findById(id).populate("writer");
+
+    if (!activity) {
+      return res.status(404).json({ message: "Activity not found" });
+    }
+
+    res.json(activity);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// mendapatkan artikel berdasarkan member
+router.get("/ma/:id", async (req, res) => {
+  const { id } = req.params; // minta id dari url 
+  try {
+    const articles = await Articles.find({ writer: id }).populate("writer");
+    if (!articles) {
+      return res
+        .status(404)
+        .json({ message: "Articles by this member is not found" });
+    }
+    res.json(articles);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// mendapatkan artikel berdasarkan type
+router.get("/type/:name", async (req, res) => {
+  const { name } = req.params; // minta type dari url frontend
+  try {
+    const articles = await Articles.find({ type: name }).populate("writer");
+    if (!articles) {
+      return res
+        .status(404)
+        .json({ message: "Articles by this member is not found" });
+    }
     res.json(articles);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const express = require("express");
 const connectDB = require("./config/db");
 const Admin = require("./models/Admin");
 const Project = require("./models/project");
-const Articles = require("./models/articles");
 
 const memberRoutes = require("./routes/memberRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const activitiesRoutes = require("./routes/activitiesRoutes");
 const articlesRoutes = require("./routes/articlesRoutes");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const uploadRoute = require('./controller/routeUpload');
+const uploadRoute = require("./controller/routeUpload");
 
 const app = express();
 const PORT = 5000;
@@ -26,7 +25,7 @@ connectDB();
 app.use(express.json());
 
 // handle image cloudinary
-app.use("/api/users" , uploadRoute);
+app.use("/api/users", uploadRoute);
 
 // endpoint untuk mendapatkan admin
 app.get("/admin", async (req, res) => {
@@ -38,76 +37,17 @@ app.get("/admin", async (req, res) => {
   }
 });
 
-// Endpoint untuk mendapatkan data anggota
-// app.get("/members", async (req, res) => {
-//   try {
-//     const members = await Member.find();
-//     res.json(members);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
 // Prefix untuk rute member
 app.use("/members", memberRoutes);
 
 // Prefix untuk rute project
-app.use("/projects", projectRoutes)
+app.use("/projects", projectRoutes);
 
 // Prefix untuk rute activity
-app.use("/activities", activitiesRoutes)
+app.use("/activities", activitiesRoutes);
 
 // Endpoint untuk mendapatkan articles
-app.use("/articles",  articlesRoutes)
-
-// Endpoint untuk mendapatkan articles berdasarkan ID
-app.get("/articles/:id", async (req, res) => {
-  const { id } = req.params; // Mendapatkan ID dari parameter URL
-  try {
-    // Menemukan proyek berdasarkan ID
-    const activity = await Articles.findById(id).populate("writer");
-
-    if (!activity) {
-      return res.status(404).json({ message: "Activity not found" });
-    }
-
-    res.json(activity); // Mengirimkan data proyek sebagai respons
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// endpoint untuk mendapatkan member articles berdasarkan ID member (MA = Member Article)
-app.get("/ma/:id", async (req, res) => {
-  const { id } = req.params; // minta id dari url frontend
-  try {
-    const articles = await Articles.find({ writer: id }).populate("writer");
-    if (!articles) {
-      return res
-        .status(404)
-        .json({ message: "Articles by this member is not found" });
-    }
-    res.json(articles);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// endpoint untuk mendapatkan article berdasarkan type
-app.get("/article/type/:name", async (req, res) => {
-  const { name } = req.params; // minta type dari url frontend
-  try {
-    const articles = await Articles.find({ type: name }).populate("writer");
-    if (!articles) {
-      return res
-        .status(404)
-        .json({ message: "Articles by this member is not found" });
-    }
-    res.json(articles);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+app.use("/articles", articlesRoutes);
 
 // CMS
 
@@ -116,9 +56,9 @@ app.delete("/delete/project/:id", async (req, res) => {
   const { id } = req.params;
   try {
     await Project.findByIdAndDelete(id);
-    res.status(200).send({ message: 'Project deleted successfully' });
+    res.status(200).send({ message: "Project deleted successfully" });
   } catch (error) {
-    res.status(500).send({ message: 'Error deleting project', error });
+    res.status(500).send({ message: "Error deleting project", error });
   }
 });
 
