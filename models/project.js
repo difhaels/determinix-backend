f4df7c264bd7723600ceb463066b7cdbc01abf28
Agung Saputra <agungsaputradifh@gmail.com>
2025-02-02 13:35:51 +0700

    updat models project

--- models/project.js
+++ models/project.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
   title: String,
-  date: String,
+  createAt: {type: Date, default: Date.now},
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Member' }],
-  img: String,
+  imgUrlBanner: String,
+  imgUrl1: String,
+  imgUrl2: String,
+  imgUrl3: String,
+  description: String,
 });
 
 const Project = mongoose.model('Project', projectSchema);
